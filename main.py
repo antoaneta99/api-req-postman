9f1c28cbc14a352638f9e2edbba9554c40d36d5a
antoaneta99 <142842628+antoaneta99@users.noreply.github.com>
2023-10-10 00:39:09 +0300

    main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import random
 
 app = Flask(__name__)
 
-##CREATE DB
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 db = SQLAlchemy()
 db.init_app(app)
 
 
-##CREATE TABLE
+
 class Cafe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=True, nullable=False)
@@ -57,14 +57,13 @@ def get_all_cafes():
 def get_cafe_at_location():
     query_location = request.args.get("loc")
     result = db.session.execute(db.select(Cafe).where(Cafe.location == query_location))
-    # Note, this may get more than one cafe per location
     all_cafes = result.scalars().all()
     if all_cafes:
         return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
     else:
         return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."}), 404
 
-# Test this inside Postman. Request type: Post ->  Body ->  x-www-form-urlencoded
+
 @app.route("/add", methods=["POST"])
 def post_new_cafe():
     new_cafe = Cafe(
@@ -83,8 +82,7 @@ def post_new_cafe():
     db.session.commit()
     return jsonify(response={"success": "Successfully added the new cafe."})
 
-# Updating the price of a cafe based on a particular id:
-# http://127.0.0.1:5000/update-price/CAFE_ID?new_price=£5.67
+
 @app.route("/update-price/<int:cafe_id>", methods=["PATCH"])
 def patch_new_price(cafe_id):
     new_price = request.args.get("new_price")
@@ -96,7 +94,7 @@ def patch_new_price(cafe_id):
     else:
         return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
 
-# Deletes a cafe with a particular id. Change the request type to "Delete" in Postman
+
 @app.route("/report-closed/<int:cafe_id>", methods=["DELETE"])
 def delete_cafe(cafe_id):
     api_key = request.args.get("api-key")
